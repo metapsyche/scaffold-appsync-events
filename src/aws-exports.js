e84ae0439d4555c275730515452c2253c1604e7a
Metapsyche <mindrogen@gmail.com>
2022-05-01 11:53:47 -0400

    initial default commit

---
+++ src/aws-exports.js
@@ -0,0 +1,9 @@
+const awsmobile =  {
+    "aws_project_region": "us-east-1",
+    "aws_appsync_graphqlEndpoint": "https://tk2mgbrxtrd5bdjz6tpcvpliza.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-4zfykxjz5vhm7cwq2f55hbnoxm",
+};
+
+export default awsmobile;
